b2569c44d7c5ddee0e60a3d58c8229a649d6b92c
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2022-05-25 19:46:44 +0200

    Add project files.

---
+++ Diak.cs
@@ -0,0 +1,22 @@
+﻿namespace CA220525_2
+{
+    internal class Diak
+    {
+        public string VezetekNev;
+        public string KeresztNev;
+        public int Magassag;
+        public float Suly;
+        public string Nem;
+
+        public Diak(string sor)
+        {
+            string[] darabok = sor.Split(' ');
+
+            this.VezetekNev = darabok[0];
+            this.KeresztNev = darabok[1];
+            this.Magassag = int.Parse(darabok[2]);
+            this.Suly = float.Parse(darabok[3]);
+            this.Nem = darabok[4];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using CA220525_2;
+
+//Iras();
+//Olvasas();
+
+using StreamReader sr = new(
+    path: "../../../res/diakok.txt",
+    encoding: Encoding.UTF8);
+
+var diakok = new Diak[1000];
+int index = 0;
+
+while (!sr.EndOfStream)
+{
+    string sor = sr.ReadLine();
+    diakok[index] = new Diak(sor);
+    index++;
+}
+
+Array.Resize(ref diakok, index);
+
+int sum = 0;
+for (int i = 0; i < diakok.Length; i++)
+{
+    sum += diakok[i].Magassag;
+}
+
+Console.WriteLine($"diákok átlagos magassága: {sum/(float)diakok.Length} cm");
+
+Console.WriteLine("\n\nkész vagyok!");
+
+
+void Iras()
+{
+    using StreamWriter sw = new(
+        path: @"../../../res/file.txt",
+        append: false,
+        encoding: Encoding.UTF8);
+
+    sw.WriteLine($"Mostmár");
+    sw.WriteLine($"csak ez");
+    sw.WriteLine($"van benne");
+}
+
+void Olvasas()
+{
+    using StreamReader sr = new(
+        path: @"../../../res/nevek.txt",
+        Encoding.UTF8);
+
+    while (!sr.EndOfStream)
+    {
+        string sor = sr.ReadLine();
+        Console.WriteLine(sor);
+    }
+}
+
